0198cdc61d0363ac6fe558de77c904b15b5cddf2
williamcoon <william.g.coon@gmail.com>
2022-03-25 09:52:49 -0600

    Update for python3

--- tcp_server.py
+++ tcp_server.py
@@ -1,40 +1,41 @@
 from twisted.internet import reactor
 from twisted.internet import protocol
 
+TCP_PORT = 8123
+
 
 class TCPProtocol(protocol.Protocol):
 
-    def __init__(self):
 
+    def __init__(self):
+        self.ack_count = 0
         pass
 
     def connectionMade(self):
-        print "New Connection"
+        print("New Connection")
 
     def connectionLost(self, reason=protocol.connectionDone):
-        print 'Connection Lost: %s' % reason
+        print(f'Connection Lost: {reason}')
         try:
-            print 'Closing connection'
+            print('Closing connection')
             self.factory.clients.remove(self)
         except ValueError:
-            print 'Client not found'
+            print('Client not found')
 
     def dataReceived(self, data):
-        if 'ECHO' in data:
-            msg = data.split(':')[1]
-            print "New ECHO: " + data
-            self.transport.write(msg)
-
-        else:
-            print "New Message: " + data
-
+        print(f"Data received: {data}")
+        self.transport.write(data=f"ACK {self.ack_count}".encode('utf-8'))
+        self.ack_count += 1
 
 def run():
     factory = protocol.Factory()
     factory.protocol = TCPProtocol
     factory.clients = []
-    reactor.listenTCP(8123, factory)
+    reactor.listenTCP(TCP_PORT, factory)
+    print(f"Listening for connections on port {TCP_PORT}")
     reactor.run()
+    print(f"Reactor closed")
+
 
 if __name__ == "__main__":
     run()
